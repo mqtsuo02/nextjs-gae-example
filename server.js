d2bbeef62af133ca62baac852377caedb221ad64
mqtsuo02 <obu.tokyo@gmail.com>
2019-04-23 18:11:53 +0900

    add express

---
+++ server.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from App Engine!");
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
+
+module.exports = app;
